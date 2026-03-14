fbd6490d530024bd331013508d2cf6c801382df7
lourduradjou <rajlourdu15@gmail.com>
2026-03-15 00:37:33 +0530

    yay for free Wagtail CMS

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
+from django.contrib.auth import get_user_model
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.dev")
-
 application = get_wsgi_application()
+User = get_user_model();
+User.objects.create_superuser('admin', 'admin@admin.com', 'password')
