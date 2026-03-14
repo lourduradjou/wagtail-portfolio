3fe596c1be6e7cbd4a433178c5c5a380779d2421
lourduradjou <rajlourdu15@gmail.com>
2026-03-15 04:37:14 +0530

    add remove utils  module

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,8 +1,27 @@
 import os
 from django.contrib.auth import get_user_model
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.dev")
 application = get_wsgi_application()
 
-from mysite.utils.create_superuser import create_superuser
-create_superuser()
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if not username or not email or not password:
+    raise ValueError("Superuser credentials not provided")
+
+if not User.objects.filter(username=username).exists():
+    print("Creating superuser...")
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password
+    )
+else:
+    print("Superuser already exists")
