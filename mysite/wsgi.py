cfea401290fcf9e9be5e9ac03312df6226384d2e
lourduradjou <rajlourdu15@gmail.com>
2026-03-15 01:27:48 +0530

    chore: remove wsgi error

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -3,5 +3,5 @@ from django.contrib.auth import get_user_model
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.dev")
 application = get_wsgi_application()
-User = get_user_model();
+User = get_user_model()
 User.objects.create_superuser('admin', 'admin@admin.com', 'password')
